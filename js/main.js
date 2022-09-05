e467efc53c4d2c2c9434b04152b8d8234db4645f
Komsha99 <58040179+Komsha99@users.noreply.github.com>
2022-09-05 11:50:06 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const menuBtn = document.querySelector('.activate');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.parentNode.classList.add('open');
+    menuBtn.parentNode.classList.remove('close');
+    setTimeout(function(){
+    menuOpen = true;
+    menuBtn.parentNode.classList.remove('close');
+    }, 500);
+    menuBtn.parentNode.classList.remove('close');
+  } else {
+    menuBtn.parentNode.classList.remove('open');
+    setTimeout(function(){
+      menuBtn.parentNode.classList.add('close')
+      menuOpen = false;
+    }, 500);
+  }
+});
+
