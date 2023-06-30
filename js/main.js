a31b973f52ae0818d090109dc3114077287d8cfc
Komsha99 <58040179+Komsha99@users.noreply.github.com>
2023-06-30 11:17:22 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -17,4 +17,18 @@ menuBtn.addEventListener('click', () => {
     }, 500);
   }
 });
-
+const body = document.querySelector("body"),
+    toggle = document.querySelector(".toggle");
+        let getMode = localStorage.getItem("mode")
+        if (getMode && getMode === "light") {
+          body.classList.add("light");
+          toggle.classList.add("active");
+        }
+    toggle.addEventListener("click", () => {
+      body.classList.toggle("light");
+        if(!body.classList.contains("light")){
+          return localStorage.setItem("mode", "dark");
+        }
+          localStorage.setItem("mode", "light");
+    })
+    toggle.addEventListener("click", () => toggle.classList.toggle("active"));
